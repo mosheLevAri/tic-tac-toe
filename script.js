dc2dd069edb5fbcdc8870db738776af8a1f8e3a4
moshe <8668871@gmail.com>
2021-12-09 16:23:30 +0200

    tic tac to

---
+++ script.js
@@ -0,0 +1,169 @@
+function player(name, img, ) {
+    return {
+        name,
+        img
+    }
+}
+let p1 = player("moshe", "x", );
+let p2 = player("avi", "o", )
+
+let currentPlayer = p1;
+
+function turnPlayer() {
+    currentPlayer = (currentPlayer == p1) ? p2 : p1;
+}
+let gameoverHTML =document.getElementsByClassName("win")
+let table = document.getElementById("table");
+let clsBox = document.getElementsByClassName("box");
+let gameoverimg = document.getElementsByClassName("gameover");
+
+let record = 10;
+let count = 0;
+let step = [];
+let item = [];
+
+let WinItem = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+table.addEventListener("click", click);
+
+
+function click(event) {
+    let target = event.target;
+    let index = Number(target.id.slice(1))
+
+    if (target.id === "table" || target.classList.contains("row") || item[index]) {
+        return
+    }
+
+    item[index] = currentPlayer.img; // save backend
+
+    target.classList.add(currentPlayer.img); // change UI
+
+    step.push(index); // save steps
+
+    count++;
+
+    let winner = CheckWiner(index)
+    if (!winner && count == 9) {
+        alert("its tie")
+    };
+
+    turnPlayer()
+};
+
+function CheckWiner(index) {
+    let arr = WinItem.filter(v => v.includes(index));
+    for (i of arr) {
+        // i = [6,7,8]
+        // i[0] == 6
+        // i[1] == 7
+        // i[2] == 8
+        if (i.every(v => item[v] == currentPlayer.img)) {
+          winning()
+          table.removeEventListener("click", click);
+            return true
+        }
+    }
+    return false
+};
+
+function winning() {
+    if (count <= record) {
+        record = count;
+        localStorage.record = JSON.stringify(record)
+    };
+    localStorage.removeItem("item");
+    localStorage.removeItem("step");
+    localStorage.removeItem("count");
+
+    const start = () => {
+        setTimeout(function() {
+            confetti.start()
+        }, 100); 
+    };
+    const stop = () => {
+        setTimeout(function() {
+            confetti.stop()
+        }, 6000);
+    };
+    start();
+    stop();  
+        gameoverimg[0].classList.add("win");
+        gameoverimg[0].innerHTML=`<h1>${currentPlayer.name}</h1><h1>is the winner !!!</h1>`; 
+};
+
+function newGame() {
+   
+    for (i = 0; i < item.length; i++) {
+        if (item[i]) {
+            clsBox[i].classList.remove("x", "o");
+        }
+    }
+    count = 0;
+    item = [];
+    step = [];
+    gameoverHTML[0].innerHTML="";
+    gameoverimg[0].classList.remove("win");
+    localStorage.removeItem("item");
+    localStorage.removeItem("step");
+    localStorage.removeItem("count");
+    table.addEventListener("click", click);
+    
+};
+
+function stepBack() {
+    if (count > 0) {
+        let idStep = Number(step[step.length - 1]);
+
+        clsBox[idStep].classList.remove("x", "o"); // change UI
+
+        item[idStep] = undefined; // save backend
+
+        step.splice(step.length - 1, 1); //  remove img  
+
+        count--; //// count -1
+
+        turnPlayer() ///  change turn (Because deletion)
+
+        return
+    }
+};
+
+function ShowRecord() {
+    let oldRecord = JSON.parse(localStorage.record);
+alert(`the recod is : ${oldRecord} step `)
+
+};
+
+function UpdateLS() {
+    localStorage.item = JSON.stringify(item);
+    localStorage.step = JSON.stringify(step);
+    localStorage.count = JSON.stringify(count);
+
+};
+
+function loudLS() {
+    let LSitem = JSON.parse(localStorage.item);
+    let LSstep = JSON.parse(localStorage.step);
+    let LScount = JSON.parse(localStorage.count);
+    item = LSitem;
+    step = LSstep;
+    count = LScount;
+    for (i = 0; i < item.length; i++) {
+        if (item[i]) {
+            clsBox[i].classList.add(`${item[i]}`)
+        }
+    }
+    return
+};
+
+
